ecad5bfe3c5463adf74c7d0a57672fc977267b15
Turki122 <129573507+Turki122@users.noreply.github.com>
2023-04-03 22:11:07 +0300

    Add files via upload

---
+++ انشاء الجداول stusdent.sql
@@ -0,0 +1,15 @@
+use school ; 
+insert into stusdent
+values (1,'ahmad',90,'male','aaa@hotmail.com'),
+  (2,'Nasser',95,'male','a1a@hotmail.com'),
+ (3,'Turki',97,'male','1aa@hotmail.com'),
+  (4,'ali',90,'male','aww@hotmail.com'),
+   (5,'nora',90,'feminine','aaffa@hotmail.com'),
+ (6,'ss',90,'male','atrreaa@hotmail.com'),
+  (7,'cc',90,'feminine','nna@hotmail.com'),
+  (8,'qq',90,'feminine','bba@hotmail.com'),
+ (9,'nn',90,'male','dda@hotmail.com'),
+ (10,'ee',90,'feminine','a3a@hotmail.com');
+         
+ /*عرض جداول */ 
+SELECT * FROM stusdent
